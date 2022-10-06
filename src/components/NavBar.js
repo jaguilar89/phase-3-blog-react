f55b444849e16d606b4d9ff7a624bdaa4751eceb
Jose Aguilar <jaguilarny@gmail.com>
2022-10-06 16:53:24 -0400

    test navbar component, add pic of schema diagram

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-    return <p>NavBar</p>
+    return (
+        <nav>
+        <NavLink>Navbar link</NavLink>
+        <NavLink>Navbar link</NavLink>
+        <NavLink>Navbar link</NavLink>
+        </nav>
+    )
 }
