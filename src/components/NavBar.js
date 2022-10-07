bff5e7449565ce894ac557d0c5988fb9f3acb1eb
Jose Aguilar <jaguilarny@gmail.com>
2022-10-07 17:32:40 -0400

    ...

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,13 @@ import { NavLink } from "react-router-dom";
 export default function NavBar() {
     return (
         <nav>
-        <NavLink>Navbar link</NavLink>
-        <NavLink>Navbar link</NavLink>
-        <NavLink>Navbar link</NavLink>
+            <ul>
+                <li>
+                    <NavLink to="home">Home</NavLink>
+                    <NavLink>Navbar link</NavLink>
+                    <NavLink>Navbar link</NavLink>
+                </li>
+            </ul>
         </nav>
     )
 }
