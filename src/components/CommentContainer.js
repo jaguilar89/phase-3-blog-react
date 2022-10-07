458e2481a49287f0f54000541566114046b00447
Jose Aguilar <jaguilarny@gmail.com>
2022-10-07 00:13:46 -0400

    add comment section to blog post

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { Button, Comment, Form, Header } from 'semantic-ui-react'
 
 export default function CommentContainer() {
     return (
-       <div id="comment-container">
-        <h1>This is a container for comments</h1>
-       </div>
+        <div id="comment-container">
+            <Header size="huge">Comments</Header>
+            <Comment.Group>
+                <Comment>
+                    <Comment.Content>
+                        <Comment.Author>Joe Schmoe</Comment.Author>
+                        <Comment.Metadata>
+                            <div>1 day ago</div>
+                        </Comment.Metadata>
+                        <Comment.Text>
+                            <p>
+                                This is a comment.
+                            </p>
+                        </Comment.Text>
+                    </Comment.Content>
+                </Comment>
+                <Comment>
+                    <Comment.Content>
+                        <Comment.Author>John Smith</Comment.Author>
+                        <Comment.Metadata>
+                            <div>2 days ago</div>
+                        </Comment.Metadata>
+                        <Comment.Text>I re-tweeted this.</Comment.Text>
+                    </Comment.Content>
+                </Comment>
+
+                <Form reply>
+                    <Form.TextArea />
+                    <Button content='Add Comment' labelPosition='left' icon='edit' primary />
+                </Form>
+            </Comment.Group>
+        </div>
     )
 }
