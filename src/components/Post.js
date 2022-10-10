e87bd428e80349d03deaad241e0df5ff71eed56f
Jose Aguilar <jaguilarny@gmail.com>
2022-10-09 21:15:42 -0400

    minor edit

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,26 +2,26 @@ import React from "react";
 import CommentContainer from "./CommentContainer";
 import { Container, Header, Button } from "semantic-ui-react";
 import { useLocation, useParams } from "react-router-dom";
+import { EditText, EditTextarea } from 'react-edit-text';
+import 'react-edit-text/dist/index.css';
 
 export default function Post() {
-    const { title } = useParams();
+    const { id } = useParams();
     const location = useLocation();
     const { postTitle, body } = location.state;
-    console.log('hello from Post.js')
 
     return (
         <div className="post">
             <Container text>
-                <Header as='h1'>Title</Header>
-                <h1>{postTitle}</h1>
+                <Header as='h1'>{postTitle}</Header>
                 <p>
                     {body}
                 </p>
-                <Button content='Edit Post' labelPosition='left' icon='edit' secondary />
-                <Button content='Delete Post' labelPosition='left' icon='trash' secondary />
             </Container>
+            <Button content='Edit Post' labelPosition='left' icon='edit' secondary />
+            <Button content='Delete Post' labelPosition='left' icon='trash' secondary />
             <Header size="huge">Comments</Header>
-            
+
             <CommentContainer />
         </div>
     )
